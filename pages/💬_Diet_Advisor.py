86637b4ea8d4fa35644dbe2ec33845f1b80ae57c
Anric Ngan <anric.ngan@duke.edu>
2024-11-02 14:18:04 -0400

    Implemented diet advisor

--- pages/💬_Diet_Advisor.py
+++ pages/💬_Diet_Advisor.py
@@ -1,5 +1,21 @@
 import streamlit as st
+from openai import OpenAI
 
 st.set_page_config(
     page_title="Diet Advisor | NutriSync"
 )
+
+st.title("Diet Advisor")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+with st.chat_message("assistant"):
+    st.write("How may I help you with  your diet today?")
+
+prompt = st.chat_input("I would like to plan a healthy lunch.")
+if prompt: 
+    st.write("Prompt: {prompt}")
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
