b94d7a39ef2fec69ec2a43625be3f02503fc1b82
Anric Ngan <anric.ngan@duke.edu>
2024-11-02 15:56:20 -0400

    Implemented Diet Advisor, Home page

--- pages/💬_Diet_Advisor.py
+++ pages/💬_Diet_Advisor.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from openai import OpenAI
+import api
 
+ai = api.AI()
 st.set_page_config(
-    page_title="Diet Advisor | NutriSync"
+    page_title = "Diet Advisor | NutriSync"
 )
 
-st.title("Diet Advisor")
-
+st.title("💬 Diet Advisor")
+st.subheader("Ask any questions you have about your diet!")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -15,7 +17,13 @@ with st.chat_message("assistant"):
     st.write("How may I help you with  your diet today?")
 
 prompt = st.chat_input("I would like to plan a healthy lunch.")
-if prompt: 
-    st.write("Prompt: {prompt}")
+
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+    with st.chat_message("assistant"):
+        response = ai.get_completion_openai(prompt)
+        st.write(response)
+
     
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.set_page_config(
     page_title="Home | NutriSync"
 )
+
+st.title("Welcome to NutriSync!")
+
+
+st.write("Use the Diet Balancer to supplement your diet with the help of LLMs!")
+
+st.write("Use the Diet Advisor for general advice on your diet!")
