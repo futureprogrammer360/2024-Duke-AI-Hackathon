ee4333013ad4efea5927e9e1bc68576e0dd76af9
Anric Ngan <anric.ngan@duke.edu>
2024-11-02 18:15:59 -0400

    Updated home page

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -10,3 +10,14 @@ st.title("Welcome to NutriSync!")
 st.write("Use the Diet Balancer to supplement your diet with the help of LLMs!")
 
 st.write("Use the Diet Advisor for general advice on your diet!")
+
+st.subheader("About")
+st.markdown(
+     """Nutritional deficiency is one of America's greatest public health challenges.
+     [According to the CDC](https://pmc.ncbi.nlm.nih.gov/articles/PMC8735562/), 
+     Only 12.3 and 10.0 percent of Americans are meeting recommended intake values for 
+      fruits and vegetables respectivly. While people can get nutritional advice from 
+      professionals, that can be unaffordable and inconvienient. NutriSync uses the 
+      power of AI to provide free and personalized nutritional advice to the public."""
+    )
+st.write("Created by Logan Chu, Ethan Fazal, Ian Le, and Anric Ngan for the 2024 Duke AI Hackathon.")
