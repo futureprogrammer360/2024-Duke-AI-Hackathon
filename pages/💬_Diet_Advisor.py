a21d1a0895665f24cdeecc968459250a2053552a
Anric Ngan <anric.ngan@duke.edu>
2024-11-02 19:10:46 -0400

    Improved diet advisor

--- pages/💬_Diet_Advisor.py
+++ pages/💬_Diet_Advisor.py
@@ -4,7 +4,8 @@ import api
 
 ai = api.AI()
 st.set_page_config(
-    page_title = "Diet Advisor | NutriSync"
+    page_title = "Diet Advisor | NutriSync",
+    page_icon = "💬"
 )
 
 st.title("💬 Diet Advisor")
@@ -13,20 +14,30 @@ st.subheader("Ask any questions you have about your diet!")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
 with st.chat_message("assistant"):
-    st.write("How may I help you with  your diet today?")
+    st.write("How may I help you with your diet today?")
 
 prompt = st.chat_input("I would like to plan a healthy lunch.")
 
 if prompt:
+
     st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.write(prompt)
         toAI = """You are a professional dietician answering the prompt given after the colon. 
-                If the prompt is unrelated to nutrition or diets, please politely answer that you 
+                If the prompt is unrelated to nutrition, diets, or health, please politely answer that you 
                 are unable to provide such information: """ + prompt
+        ai.messages.append({"role": "user", "content": toAI})
+
     with st.chat_message("assistant"):
         response = ai.get_completion_openai(toAI)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        ai.messages.append({"role": "assistant", "content": response})
         st.write(response)
 
     
